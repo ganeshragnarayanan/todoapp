e3cad4ddd6f4e88256a59fb00d72d5915cfcc52f
Ganesh <ganesh.rag.narayanan@hpe.com>
2017-08-18 19:22:28 -0700

    todo app
    Signed-off-by: Ganesh <ganesh.rag.narayanan@hpe.com>

---
+++ app/src/main/java/com/example/ganesh/simpletodo/db/TaskContract.java
@@ -0,0 +1,18 @@
+package com.example.ganesh.simpletodo.db;
+
+/**
+ * Created by GANESH on 8/18/17.
+ */
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.ganesh.simpletodo.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
